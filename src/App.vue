bcb6ae5494198b516546de1d5554009760d89ea1
Gullrye <wmwspecter@163.com>
2021-08-29 17:23:56 +0800

    add detail-animation

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <!-- keep-alive 主要用于保留组件状态或避免重新渲染 -->
     <!-- 当组件在 <keep-alive> 内被切换，它的 activated 和 deactivated 这两个生命周期钩子函数将会被对应执行。 -->
-    <keep-alive>
+    <keep-alive exclude="Detail">
       <router-view/>
     </keep-alive>
   </div>
