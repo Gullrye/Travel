3e65fc15b68d14c2f06361470ebd4ac939b18b08
Gullrye <wmwspecter@163.com>
2021-08-29 15:58:41 +0800

    add detail-list

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,25 +2,38 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
+    <div class="content">
+      <detail-list :list='list'></detail-list>
+    </div>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner.vue'
 import DetailHeader from './components/Header.vue'
+import DetailList from './components/List.vue'
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
   },
   data () {
-    return {}
+    return {
+      list: [
+        {title: '成人票1'},
+        {title: '成人票2', children: [{title: '成人票2-1', children: [{title: '成人票2-1-1'}]}]},
+        {title: '成人票3', children: [{title: '成人票3-1'}]},
+        {title: '成人票4', children: [{title: '成人票4-1'}]},
+        {title: '成人票5', children: [{title: '成人票5-1'}]}
+      ]
+    }
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-div
-  height 50rem
+.content
+  height 20rem
 </style>
