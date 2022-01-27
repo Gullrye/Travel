8385d5559ef7461948d4114bcfabdf2023095b97
Gullrye <wmwspecter@163.com>
2022-01-27 17:20:55 +0800

    fix: api config

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -35,7 +35,7 @@ export default {
   },
   methods: {
     getCityInfo () {
-      axios.get('/api/city.json').then(this.handleGetCityInfoSucc)
+      axios.get('/travel/api/city.json').then(this.handleGetCityInfoSucc)
     },
     handleGetCityInfoSucc (res) {
       res = res.data
